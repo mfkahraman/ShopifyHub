ff2cf189320209a8609c058494d5d3e52ec94586
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-12-29 20:16:54 +0300

    Add project files.

---
+++ ShopifyHub.Application/Services/StripeService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopifyHub.Application.Services
+{
+    internal class StripeService
+    {
+    }
+}
---
+++ ShopifyHub.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopifyHub.Infrastructure.Data
+{
+    internal class AppDbContext
+    {
+    }
+}
